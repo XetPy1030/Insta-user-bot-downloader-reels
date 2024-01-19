ca2e2fc60dd715800403a96fc6c026fa578fe107
XetPy1030 <xetpy1030@mail.ru>
2024-01-19 22:20:40 +0300

    V2

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from telethon import TelegramClient, events
-from telethon.tl.types import Message
-import re
-from dotenv import load_dotenv
 import os
-import downloaders
+import re
+
+from dotenv import load_dotenv
+from telethon import TelegramClient, events
+from telethon.tl.types import PeerChat
+
+import seledka
 
 load_dotenv()
 
@@ -20,11 +22,24 @@ insta_reel_link = re.compile(r'https://?(www)?.instagram.com/reel/.*?/')
 
 client.start()
 
+main_group_id = None
+
+chat_ids_for_forwards = [
+    -1001396952381,
+    -1001298257907,
+    -1001162036880,
+    -1001755311192,
+    -1001583795567,
+    -1001321956720,
+    -1001678254511,
+    -1001509411348,  # Алабуга политех
+]
+
 
 @client.on(events.NewMessage(pattern='/insta'))
 async def my_event_handler(event):
     # получение смс на которое отвечают
-    message: Message = event.message
+    message = event.message
     if message.reply_to is None:
         return
     message_reply_obj = await message.get_reply_message()
@@ -40,18 +55,69 @@ async def my_event_handler(event):
     link = insta_link_obj.group(0)
     await client.send_message(message.peer_id, 'Скачивание файла...')
     try:
-        downloaders.download_1(link)
+        seledka.download_video(seledka.get_video_url(link))
     except Exception as ex:
-        await client.send_message(message.peer_id,
-                                  'Неудачное скачивание через первый источник...')
-        try:
-            downloaders.download_2(link)
-        except Exception as ex:
-            await client.send_message(message.peer_id, 'Через второй тоже...')
-            raise ex
+        await client.send_message(
+            message.peer_id,
+            'Неудачное скачивание через первый источник...'
+        )
+        raise ex
 
     await client.send_message(message.peer_id, 'Отправление файла...')
     await client.send_file(message.peer_id, 'video.mp4')
+
+
+@client.on(
+    events.NewMessage(pattern='https://?(www)?.instagram.com/reel/.*?/')
+)
+async def my_event_handler(event):
+    if main_group_id is None:
+        return
+
+    if event.message.peer_id.chat_id != main_group_id:
+        return
+
+    # получение смс на которое отвечают
+    message = event.message
+
+    # поиск ссылки на инсту
+    insta_link_obj = insta_reel_link.search(message.text)
+    if insta_link_obj is None:
+        return
+
+    link = insta_link_obj.group(0)
+    await client.send_message(message.peer_id, 'Скачивание файла...')
+    try:
+        seledka.download_video(seledka.get_video_url(link))
+    except Exception as ex:
+        await client.send_message(
+            message.peer_id,
+            'Неудачное скачивание через первый источник...'
+        )
+        raise ex
+
+    await client.send_message(message.peer_id, 'Отправление файла...')
+    await client.send_file(message.peer_id, 'video.mp4')
+
+
+@client.on(events.NewMessage(pattern='/set_main_group'))
+async def my_event_handler(event):
+    global main_group_id
+    message = event.message
+
+    peer_chat: PeerChat = message.peer_id
+
+    main_group_id = peer_chat.chat_id
+    await client.send_message(message.peer_id, 'Главная группа установлена')
+
+
+# если сообщение из одной из групп для пересылки
+@client.on(events.NewMessage(chats=chat_ids_for_forwards))
+async def my_event_handler(event):
+    message = event.message
+    if main_group_id is None:
+        return
+    await client.send_message(main_group_id, message)
 
 
 client.run_until_disconnected()
---
+++ seledka.py
@@ -0,0 +1,32 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+
+
+def get_video_url(url):
+    browser = webdriver.Firefox(
+        service=Service(
+            executable_path='geckodriver'
+        )
+    )
+    try:
+        browser.get(url)
+        browser.implicitly_wait(5)
+        elem = browser.find_element(By.TAG_NAME, 'video')
+        video_url = elem.get_attribute('src')
+        return video_url
+    finally:
+        browser.quit()
+
+
+def download_video(video_url):
+    resp = requests.get(video_url, stream=True)
+    with open('video.mp4', 'wb') as f:
+        for chunk in resp.iter_content(chunk_size=8192):
+            f.write(chunk)
+
+
+if __name__ == '__main__':
+    vid_url = get_video_url('https://www.instagram.com/reel/C2KVKh8MKYg/')
+    download_video(vid_url)
